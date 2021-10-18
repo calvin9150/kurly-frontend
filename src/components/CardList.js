3811f4403efafc0ae25be91f1d2091b69a829172
calvin9150 <wonbae9150@gmail.com>
2021-10-19 01:53:13 +0900

    [feat ]메인페이지 구현

---
+++ src/components/CardList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '../elements/Card';
+
+const CardList = () => {
+	return (
+		<>
+			<Card />
+		</>
+	);
+};
+
+export default CardList;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const Main = props => {
+import CardList from '../components/CardList';
+import Event from '../components/Event';
+import ProductSlick from '../components/ProductSlick';
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	width: 100vw;
+	/* height: 100vh; */
+`;
+
+const ProductSlickLayout = styled.div`
+	width: 53%;
+	height: 500px;
+	margin: 80px auto;
+`;
+
+const Main = () => {
+	// const dispatch = useDispatch();
+
 	return (
 		<>
-			<div>메인 브랜치</div>
-			<div>테스트!!</div>
+			<Container>
+				<Event />
+				<ProductSlickLayout>
+					<ProductSlick />
+				</ProductSlickLayout>
+				<CardList />
+			</Container>
 		</>
 	);
 };
