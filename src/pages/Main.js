bc1a85dd4245f7b6d1ead18f9149afa40fd258ad
calvin9150 <wonbae9150@gmail.com>
2021-10-18 12:53:07 +0900

    브랜치 테스트1

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-const Main = (props) => {
-  return <div>메인</div>;
+const Main = props => {
+	return <div>메인 브랜치</div>;
 };
 
 export default Main;
